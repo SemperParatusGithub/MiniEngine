4f1752e74578044e48923498bdedb48325b6ca73
SemperParatusGithub <schlagernils@gmail.com>
2021-04-11 14:39:09 +0200

    Hello Triangle

---
+++ Scripts/Build/Test.glsl
@@ -0,0 +1,25 @@
+#shader vertex
+#version 450 core
+
+layout(location = 0) in vec3 a_Position;
+layout(location = 1) in vec4 a_Color;
+
+out vec4 v_Color;
+
+void main()
+{
+	v_Color = a_Color;
+	gl_Position = vec4(a_Position, 1.0);
+}
+
+#shader fragment
+#version 450 core
+
+layout(location = 0) out vec4 o_Color;
+
+in vec4 v_Color;
+
+void main()
+{
+	o_Color = v_Color;
+}
