3cf0f5798da196fc9c3d104f27da2eed92bdba86
SemperParatusGithub <schlagernils@gmail.com>
2021-10-30 21:56:02 +0200

    UUIDs

---
+++ Engine/Source/Core/UUID.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "EngineBase.h"
+
+#include <random>
+
+
+namespace Engine
+{
+	class UUID
+	{
+	public:
+		UUID();
+		UUID(const UUID&) = default;
+		~UUID() = default;
+
+		inline operator u64() const
+		{
+			return m_UUID;
+		}
+
+	private:
+		u64 m_UUID;
+
+		static std::random_device					s_RandomDevice;
+		static std::mt19937_64						s_Gen;
+		static std::uniform_int_distribution<u64>	s_UniformDistribution;
+	};
+}
